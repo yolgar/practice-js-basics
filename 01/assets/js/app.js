53a4d8d5558681b114f21297dcff517b738b4fe2
Olga Yepikhina <olga.yepikhina@gmail.com>
2023-08-21 20:05:26 +0200

    Finish 01

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -2,3 +2,26 @@ const a = '4.2';
 const b = 9;
 
 console.log(a, b);
+console.log(typeof a, typeof b);
+
+const add = parseInt(a) + b;
+console.log(add);
+
+const sub = a - b;
+console.log(sub);
+
+const mult = a * b;
+console.log(mult.toFixed(2));
+
+const div = a / b;
+console.log(div);
+
+const arr = [add, sub, mult, div];
+
+arr.forEach(function(num) {
+    if(num >= 20) {
+        console.log(num + ' jest większe od 20');
+    } else {
+        console.log(num + ' jest mniejsze lub rowne');
+    }
+})

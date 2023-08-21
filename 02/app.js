08b73391ab356ceb6ec72b8a656a896de9469c0c
Olga Yepikhina <olga.yepikhina@gmail.com>
2023-08-21 20:52:50 +0200

    Finish 02

--- 02/app.js
+++ 02/app.js
@@ -1,7 +1,34 @@
 
 /* rozwiązanie z pętlą for */
-const x = 4;
+// const x = Number(prompt('Podaj liczbę!'));
 
+// if(x >= 1 && x < 10) {
+// for(let i=1; i<=9; i++) {
+// const result = x * i;
+// console.log(x + ' x ' + i + ' = ' + result);
+// }
+// } else {
+//     console.error('Podałeś błędne dane');
+// }
 
 
 /* rozwiązanie z pętlą while  */
+const a = prompt('Podaj liczbę!');
+const n = prompt('Podaj potegę!');
+
+let iter = 0;
+let result = 1;
+let info = '';
+while(iter < n){
+    result = result * a;
+
+    if(iter > 0) {
+    info = info + ' * ';
+    }
+    info = info + a;
+
+    iter ++;
+}
+info = info + ' = ' + result;
+    console.log(result);
+    console.log(info);

2af395f9c7ff1f6488dca8d447f2bbb1ccc6dbbf
Olga Yepikhina <olga.yepikhina@gmail.com>
2023-08-21 20:56:42 +0200

    Adjust 02

--- 02/app.js
+++ 02/app.js
@@ -1,21 +1,22 @@
 
 /* rozwiązanie z pętlą for */
-// const x = Number(prompt('Podaj liczbę!'));
+const x = Number(prompt('Podaj liczbę!'));
 
-// if(x >= 1 && x < 10) {
-// for(let i=1; i<=9; i++) {
-// const result = x * i;
-// console.log(x + ' x ' + i + ' = ' + result);
-// }
-// } else {
-//     console.error('Podałeś błędne dane');
-// }
+if(x >= 1 && x < 10) {
+for(let i=1; i<=9; i++) {
+const result = x * i;
+console.log(x + ' x ' + i + ' = ' + result);
+}
+} else {
+    console.error('Podałeś błędne dane');
+}
 
 
 /* rozwiązanie z pętlą while  */
-const a = prompt('Podaj liczbę!');
-const n = prompt('Podaj potegę!');
+const a = Number(prompt('Podaj liczbę z przedziału od 1 do 100!'));
+const n = Number(prompt('Podaj potegę z przedziału od 0 do 10!'));
 
+if(a >= 1 && a <= 100 && n >= 0 && n <= 10){
 let iter = 0;
 let result = 1;
 let info = '';
@@ -23,7 +24,7 @@ while(iter < n){
     result = result * a;
 
     if(iter > 0) {
-    info = info + ' * ';
+    info += ' * ';
     }
     info = info + a;
 
@@ -32,3 +33,6 @@ while(iter < n){
 info = info + ' = ' + result;
     console.log(result);
     console.log(info);
+} else {
+    alert('Podałeś dane poza zakresem');
+}

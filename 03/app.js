9988acf2ed5f875c4ddc629ecd106ce79d891288
Olga Yepikhina <olga.yepikhina@gmail.com>
2023-08-22 20:24:07 +0200

    Finish 03

--- 03/app.js
+++ 03/app.js
@@ -7,10 +7,53 @@ const c = randomNumber(min, max);
 
 console.log(a, b, c);
 
+function getSum (d, e, k) {
+    const dInt = parseInt(d);
+    const eInt = parseInt(e);
+    const kInt = parseInt(k);
+
+    const arr = [dInt, eInt, kInt];
+    arr.sort(function(x,y) {
+        return y - x;
+    });
+    return arr[0] + arr[1];
+}
+
+const sum = getSum(a, b, c);
+console.log(sum);
+
+
+const isEven = function(num) {
+if(typeof num != 'number'){
+    return null;
+} 
+return num % 2 === 0;
+}
+// console.log(isEven(9));
+
+const even = isEven(sum);
+console.log(even);
 
 
 
 
+function showInfo(value, isEven) {
+    switch(isEven) {
+case null:
+    console.log('Podany argument ' + value +' nie jest liczbą');
+    break;
+case true:
+    console.log('Podany argument ' + value +' jest parzysty');
+    break;
+case false:
+    console.log('Podany argument ' + value +' jest nieparzysty');
+    break;
+default:
+    console.log('Błędne dane');
+    break;
+}
+}
+showInfo(sum, even);
 
 
 function randomNumber(min, max) {

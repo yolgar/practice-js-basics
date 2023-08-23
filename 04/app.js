4f87d887039b003feef71b5badd5cbb07c5b8b51
Olga Yepikhina <olga.yepikhina@gmail.com>
2023-08-23 20:05:35 +0200

    FInish 04

--- 04/app.js
+++ 04/app.js
@@ -1,3 +1,9 @@
+const arr = createArray(10, 200);
+const largest = getLargest(arr);
+const avg = getAvg(largest);
+console.log(avg);
+
+
 function createArray(min, max) {
     const arr = [];
     for(let i = 0; i < 20; i++) {
@@ -8,3 +14,38 @@ function createArray(min, max) {
 return arr;
 }
 console.log(createArray(5,90));
+
+
+function getLargest(arr) {
+   arr.sort(function(x,y) {
+    return y - x;
+});
+    return arr.slice(0, 10);
+}
+console.log(getLargest(createArray(1,20)));
+
+
+// let total = 0;
+// let count = 0;
+
+// function getAvg(arr){
+//     if(arr.length === 0){
+//                 return 0;
+//             }
+// arr.forEach(function(index, value){
+//     total += value;
+//     count++;
+// });
+// return total / count;
+// }
+
+function getAvg(arr){
+    if(arr.length === 0){
+        return 0;
+    }
+    const sum = arr.reduce(function(acc, curr){
+    return acc + curr;
+    }, 0);
+    return sum / arr.length; 
+}
+console.log(getAvg(createArray(1,9)));
